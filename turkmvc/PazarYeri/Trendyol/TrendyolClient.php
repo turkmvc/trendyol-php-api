42da14096cfa85aa8a7809089404f06a62cf7d53
Cuma KÖSE <25532765+turkmvc@users.noreply.github.com>
2021-01-31 18:08:23 +0300

    Create TrendyolClient.php

---
+++ turkmvc/PazarYeri/Trendyol/TrendyolClient.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace turkmvc\PazarYeri\Trendyol;
+
+use turkmvc\PazarYeri\Trendyol\Helper\Gateway;
+
+Class TrendyolClient extends Gateway
+{
+
+	/**
+	 *
+	 * @description Trendyol Api SupplierId
+	 * @param string $apiSupplierId
+	 *
+	 */
+	public function setSupplierId($apiSupplierId)
+	{
+		$this->apiSupplierId = $apiSupplierId;
+	}
+
+	/**
+	 *
+	 * @description Trendyol Api Kullanıcı Adı
+	 * @param string $apiUsername
+	 *
+	 */
+	public function setUsername($apiUsername)
+	{
+		$this->apiUsername = $apiUsername;
+	}
+
+	/**
+	 *
+	 * @description Trendyol Api Şifre
+	 * @param string $apiPassword
+	 *
+	 */
+	public function setPassword($apiPassword)
+	{
+		$this->apiPassword = $apiPassword;
+	}
+
+}
